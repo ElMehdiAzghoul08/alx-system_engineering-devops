78a36f1049df2f070e0afa711bdfb82263a6576b
El Mehdi <azghoulmehdi899@gmail.com>
2024-05-21 01:55:54 +0100

    task0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,32 +9,25 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <id_>")
-    else:
-        try:
-            id_ = int(sys.argv[1])
+    id_ = sys.argv[1]
 
-            url_usr_ = f"https://jsonplaceholder.typicode.com/users/{id_}"
-            url_tds_ = f"https://jsonplaceholder.typicode.com/todos?\
+    url_usr_ = f"https://jsonplaceholder.typicode.com/users/{id_}"
+    url_tds_ = f"https://jsonplaceholder.typicode.com/todos?\
                     userId={id_}"
 
-            res_usr_ = requests.get(url_usr_)
-            data_usr_ = res_usr_.json()
+    res_usr_ = requests.get(url_usr_)
+    data_usr_ = res_usr_.json()
 
-            res_tds_ = requests.get(url_tds_)
-            data_tds_ = res_tds_.json()
+    res_tds_ = requests.get(url_tds_)
+    data_tds_ = res_tds_.json()
 
-            emp_name_ = data_usr_.get('name')
+    emp_name_ = data_usr_.get('name')
 
-            tot_tasks_ = len(data_tds_)
-            tasks_finished_ = [t for t in data_tds_ if t.get('completed')]
-            num_tasks_finished_ = len(tasks_finished_)
+    tot_tasks_ = len(data_tds_)
+    tasks_finished_ = [t for t in data_tds_ if t.get('completed')]
+    num_tasks_finished_ = len(tasks_finished_)
 
-            print(f"Employee {emp_name_} is done with tasks(\
-                    {num_tasks_finished_}/{tot_tasks_}):")
-            for t in tasks_finished_[:11]:
-                print(f"\t {t.get('title')}")
-
-        except ValueError:
-            print("The employee ID must be an integer.")
+    print(f"Employee {emp_name_} is done with tasks(\
+            {num_tasks_finished_}/{tot_tasks_}):")
+    for t in tasks_finished_[:11]:
+        print(f"\t {t.get('title')}")
