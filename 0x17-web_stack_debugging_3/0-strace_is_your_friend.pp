51b01dd61132c11794cdacd8010d550198a48145
El Mehdi <azghoulmehdi899@gmail.com>
2024-06-06 00:11:37 +0100

    task0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#-Strace is your friend Task
+
+exec {'file_rep_':
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
